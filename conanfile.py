73e5e7ceaec8764a3fb67ee14bedb79aed2ebeda
Björn Stresing <bjoern.stresing@gmx.de>
2021-02-25 21:49:26 +0100

    Fix ci

--- conanfile.py
+++ conanfile.py
@@ -49,11 +49,8 @@ class MSYS2Conan(ConanFile):
         elif self.settings.arch_build == "x86_64":
             url = "https://sourceforge.net/projects/msys2/files/Base/i686/msys2-base-i686-%s.tar.xz" % self.version
         filename = self._download(url)
-        tar_name = filename.replace(".xz", "")
-        self.run("7z.exe x {0} -y".format(filename))
-        self.run("7z.exe x {0} -y".format(tar_name))
+        self.run("7z.exe x {0}".format(filename))
         os.unlink(filename)
-        os.unlink(tar_name)
 
         packages = []
         if self.options.packages:
